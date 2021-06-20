6c44c39e92e309cf2871a467237ec7ee857c4065
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-06-20 21:33:38 +0200

    Add rotation for card when clicking

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,11 @@ let card = document.getElementsByClassName('card');
 let cards = [...card];
 
 for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', displayCard);
+    cards[i].addEventListener('click', flipCard);
 };
 
+function flipCard() {
+    this.classList.toggle('open');
+    this.classList.toggle('show');
+    this.classList.toggle('disabled');
+}
