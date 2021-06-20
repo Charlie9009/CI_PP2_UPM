783e1a74276bca7318139d9566bfbfc9ff62d300
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-06-20 20:50:37 +0200

    Add card and loop for cards in js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,8 @@
-const allCards = document.querySelectorAll('.card');
+// Add a card array and a loop to add eventListener to cards
+let card = document.getElementsByClassName('card');
+let cards = [...card];
 
-function flipCard() {
-    this.classList.toggle('flip');
-}
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', displayCard);
+};
 
-allCards.forEach(cards => cards.addEventListener('click', flipCard));
