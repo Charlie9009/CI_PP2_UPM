75cdb0935a7311c8c4fcc4725b853b1308b1cb10
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-06-21 21:19:23 +0200

    Add data names for images and identification for cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,8 +6,20 @@ for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', flipCard);
 };
 
+let flippedCard = false;
+let firstCardChoice, secondCardChoice;
+
 function flipCard() {
     this.classList.toggle('open');
     this.classList.toggle('show');
     this.classList.toggle('disabled');
+
+    if (!flippedCard) {
+        flippedCard = true;
+        firstCardChoice = this;
+    } else {
+        flippedCard = false;
+        secondCardChoice = this;
+
+    }
 }
