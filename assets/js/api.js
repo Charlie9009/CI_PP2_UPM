24c62381700f999b8f8e3354c6f21036d0dc3deb
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-04 14:28:02 +0200

    Add error code

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,17 @@
 function fetchData() {
-  fetch('https://reqres.in/api/users').then(response => {
-    
+  fetch('https://reqres.in/api/users')
+  .then(response => {
+    if (!response.ok) {
+      throw Error('ERROR')
+    }
+    return response.json();
   })
+  .then(data => {
+    console.log(data.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 }
+
+fetchData();
