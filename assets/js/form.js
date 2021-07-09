78354f1aeb774e4dc2abfc8f078e4609e1e153ee
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-10 00:01:52 +0200

    Add responsive design to form submit button

--- assets/js/form.js
+++ assets/js/form.js
@@ -8,8 +8,7 @@ const email = document.getElementById('email');
 const form = document.getElementById('form');
 
 function validateForm() {
-    let name = document.forms['form']['full-name'].value;
-    let email = document.forms['form']['email'].value;
+
     if (name === '') {
         alert('Name must be filled out');
     } else if (email === '') {
