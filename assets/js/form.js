9237ba96ab391b59e4fac5f66ab73c63b3ceb55b
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-10 20:01:08 +0200

    Add comments to form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,15 +1,13 @@
 /**
  * The form.js is used by the contact.html file to validate the form.
  * It also uses emailJS as a tool to send the filled out form to the site owners gmail.
+ * The function validateForm informs the user if they have not filled it out the form and also informs them if they did it correctly.
+ * The variables name and email are targeted by their value.
  */
-
-const name = document.getElementById('full-name');
-const email = document.getElementById('email');
-const form = document.getElementById('form');
-
 function validateForm() {
-    let name = document.forms['form']['full-name'].value;
-    let email = document.forms['form']['email'].value;
+    const name = document.querySelector('#full-name').value;
+    const email = document.querySelector('#email').value;
+    // If statements to see if name and email has been filled out. If they have fire sendEmail function. If they have not return alert.
     if (name === '') {
         alert('Name must be filled out');
     } else if (email === '') {
@@ -20,15 +18,20 @@ function validateForm() {
     return false;
 }
 
+/**
+ * Get the form and add an eventListener to listen for the submit.
+ * If the submit is fired send email thru emailJS.
+ */
 function sendEmail() {
     document.getElementById('form').addEventListener('submit', function (event) {
         event.preventDefault();
         // ID from emailJS
-        emailjs.init('user_ijKY6mblVUbu5AeWdbYlk')
+        emailjs.init('user_ijKY6mblVUbu5AeWdbYlk');
         emailjs.send("gmail", "us_memory", {
+            // User name and email that has been filled out.
             user_name: form.name.value,
             user_email: form.email.value,
         });
-        alert('Your form was sent!')
+        alert('Your form was sent!');
     });
-};
+}
