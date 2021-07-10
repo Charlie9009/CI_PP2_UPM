c170511439ed7a83e59403df3bc7544e3c5b92f9
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-10 13:42:24 +0200

    Add form validation back

--- assets/js/form.js
+++ assets/js/form.js
@@ -8,7 +8,8 @@ const email = document.getElementById('email');
 const form = document.getElementById('form');
 
 function validateForm() {
-
+    let name = document.forms['form']['full-name'].value;
+    let email = document.forms['form']['email'].value;
     if (name === '') {
         alert('Name must be filled out');
     } else if (email === '') {
