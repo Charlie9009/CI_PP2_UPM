716b5b6adb4611d9bd84498e4362870882211c13
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-07-11 16:07:03 +0200

    Add two new functions with loop to remove class when clicking close button

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,16 +4,16 @@
  * The function validateForm informs the user if they have not filled it out the form and also informs them if they did it correctly.
  * The variables name and email are targeted by their value.
  */
- let modalForm = document.getElementById('formModal')
+ let modalForm = document.getElementById('formModal');
 
 function validateForm() {
     const name = document.querySelector('#full-name').value;
     const email = document.querySelector('#email').value;
     // If statements to see if name and email has been filled out. If they have fire sendEmail function. If they have not return alert.
     if (name === '') {
-        modalForm.classList.add('form-modal-show-name')
+        modalForm.classList.add('form-modal-show-name');
     } else if (email === '') {
-        modalForm.classList.add('form-modal-show-email')
+        modalForm.classList.add('form-modal-show-email');
     } else {
         sendEmail();
     }
@@ -34,6 +34,17 @@ function sendEmail() {
             user_name: form.name.value,
             user_email: form.email.value,
         });
-        modalForm.classList.add('form-modal-show-send')
+        modalForm.classList.add('form-modal-show-send');
     });
+}
+
+let getFormButton = document.getElementsByClassName('close-form-button');
+
+function closeFormModal() {
+for (var i = 0; i < getFormButton.length; i++) {
+    getFormButton[i].addEventListener('click', removeClass);
+}}
+
+function removeClass() {
+    modalForm.classList.remove('form-modal-show-name');
 }
